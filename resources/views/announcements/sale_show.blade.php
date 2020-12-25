91e3563b1edbd28f7affa5aee0ad5a31d8d0183f
Teymur <ni-ttg@hotmail.com>
2020-12-26 00:08:47 +0400

    Refactor saleShow.blade info details

--- resources/views/announcements/sale_show.blade.php
+++ resources/views/announcements/sale_show.blade.php
@@ -30,19 +30,26 @@
                     ></sale-image-show>
                 </div><!--Seller Store image div-->
 
-                <div class="p-3 sale__after-images d-flex">
-                    <div class="ml-5 text-white d-inline-block sale__main-price">
-                        {{ $sale->price }}
-                        <span class="text-subtitle sale__price-subtitle">AZE</span>
-                    </div>
-                    <div class="sale__main-title">
-                        {{ $sale->title }}
+                <div class="col-12 p-xl-3 p-lg-3 px-md-0 px-sm-0 py-md-3 py-sm-3 sale__after-images d-flex">
+                    <div class="row col-xl-11 col-lg-11 col-md-12 col-sm-12 m-auto">
+                        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-4 pl-sm-0 text-center text-md-left text-sm-left">
+                            <div class="text-white d-inline-block sale__main-price">
+                                {{ $sale->price }}
+                                <span class="text-subtitle sale__price-subtitle">AZE</span>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-6 col-lg-6 col-md-8 col-sm-8 mt-3 pl-0">
+                            <div class="sale__main-title">
+                                {{ $sale->title }}
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div class="col-lg-12 d-block m-auto">
-                    <div class="col-lg-7 d-block m-auto sale__info">
-                        <div class="sale__info-content">
+                <div class="row col-xl-11 col-lg-11 col-md-12 col-sm-12 px-md-0 px-sm-0 m-auto">
+                    <div class="col-xl-8 col-lg-8 order-lg-last col-md-12 col-sm-12 sale__info">
+                        <div class="pl-2 sale__info-content">
                             <div class="border-bottom border-info d-inline-block mb-2 px-4 text__bold">
                                 <span class="">{{ $sale->marka }}</span>
 
@@ -57,13 +64,77 @@
                                 {{ $sale->condition }}
                             </div>
 
-                            <div class="border-bottom border-info d-inline-block px-4 text__bold">
-                                {{ $sale->city }}
+                            <div class="border-bottom border-info d-inline-block px-4 d-flex text__bold">
+                                <div class="">
+                                    {{ $sale->city }}
+                                </div>
                             </div>
                         </div>
 
                         <div class="d-inline-block px-4 py-3 sale__note">
                             {{ $sale->note }}
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 order-lg-first text-center">
+                        <div class="col-xl-12 col-lg-12 mt-3 pt-2 mx-auto d-flex sale__left-info">
+                            <div class="my-auto mr-3 sale__phone-bg text-success">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-telephone-forward-fill" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zm10.761.135a.5.5 0 0 1 .708 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L14.293 4H9.5a.5.5 0 0 1 0-1h4.793l-1.647-1.646a.5.5 0 0 1 0-.708z"/>
+                                </svg>
+                            </div>
+
+                            <div class="">
+                                <div class="mb-n1">
+                                    <a href="tel:(050)2536564" class="text-dark">
+                                        (050)2536564
+                                    </a>
+                                </div>
+
+                                <div class="">
+                                    <a href="" class="sale__name">
+                                        {{ $sale->name }}
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-12 col-lg-12 mt-3 mx-auto py-2 d-flex sale__left-info">
+                            <div class="d-flex text-success">
+                                <span class="mr-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart2" viewBox="0 0 16 16">
+                                        <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+                                    </svg>
+                                </span>
+
+                                <div class="">
+                                    <a href="">
+                                        <span class="sale__name">
+                                            {{ $sale->name }}
+                                        </span>
+
+                                        <span class=".sale__name-sub">
+                                            bütün elanlar
+                                        </span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="text-left text-black-50 mt-4 sale__last-info">
+                            <div class="">
+                                <span class="">Elanın nömrəsi:</span>
+                                <span class="">19038581</span>
+                            </div>
+                            <div class="">
+                                <span class=""></span>
+                                <span class="">Baxışların sayı:</span>
+                                <span class="">75651</span>
+                            </div>
+                            <div class="">
+                                <span class="">Yeniləndi:</span>
+                                <span class="">Bugün, 09:21</span>
+                            </div>
                         </div>
                     </div>
                 </div>
